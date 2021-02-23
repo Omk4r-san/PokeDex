6b2ed0d934940cb394f69b5232d9353334e4a61a
Omk4r-san <74708985+Omk4r-san@users.noreply.github.com>
2021-02-24 00:00:19 +0530

    App created

---
+++ PokeDex/lib/Api/ApiClass.dart
@@ -0,0 +1,37 @@
+import 'package:PokeDex/Models/PokemonDetails_model.dart';
+import 'package:PokeDex/Models/PokemonModel.dart';
+import 'package:http/http.dart' as http;
+
+class ApiManager {
+  Future<PokemonModel> getPokemon() async {
+    String url =
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+    var response = await http.get(url);
+    try {
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        final pokemon = pokemonModelFromJson(jsonString.toString());
+        return pokemon;
+      }
+    } catch (Exception) {
+      print(Exception.toString());
+    }
+    return null;
+  }
+
+  Future<PokemonDetailModel> getPokemonDetail() async {
+    String url = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
+    var response = await http.get(url);
+    try {
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        final pokemonDetail = pokemonDetailModelFromJson(jsonString.toString());
+        return pokemonDetail;
+      }
+    } catch (Exception) {
+      print(Exception.toString());
+    }
+    return null;
+  }
+}
---
+++ PokeDex/lib/Home.dart
@@ -0,0 +1,92 @@
+import 'package:PokeDex/Api/ApiClass.dart';
+import 'package:PokeDex/Models/PokemonModel.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color customcolor = Color(0xffffffff);
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Title(
+          color: Colors.teal,
+          child: Center(child: Text("PokeDex")),
+        ),
+      ),
+      body: FutureBuilder<PokemonModel>(
+        future: ApiManager().getPokemon(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return mainPage(snapshot.data.pokemon);
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Widget mainPage(data) {
+    return GridView.builder(
+        itemCount: data.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: (2)),
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PokemonDetail(
+                          data: data[index],
+                        ))),
+            child: Card(
+              shape: RoundedRectangleBorder(),
+              elevation: 5,
+              child: Container(
+                height: 180,
+                width: MediaQuery.of(context).size.width / 2,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      child: CachedNetworkImage(
+                        imageUrl: data[index].img,
+                        placeholder: (context, url) =>
+                            Center(child: CircularProgressIndicator()),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
+                      ),
+                    ),
+                    Text(data[index].name)
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
+
+class PokemonDetail extends StatelessWidget {
+  final Map<String, dynamic> data;
+
+  const PokemonDetail({Key key, this.data}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    print(data);
+    return Container();
+  }
+}
